9886b4b681cbaa5f1291509b3a25495b83e8f5aa
sagge14 <sagge14@yandex.ru>
2026-02-07 22:12:40 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include "lib.h"
+
+#include <iostream>
+#include "src\MyMapAllocator.hpp"
+
+
+struct alignas(64) OverAligned {
+    std::uint64_t x[8];
+};
+
+int main()
+{
+    using value = OverAligned;
+    std::cout << "Version: " << version() << std::endl;
+
+    MyMapAllocator<std::pair<const int, value>> alloc(1000000);
+
+
+    std::map<
+            int,
+            value,
+            std::less<int>,
+            MyMapAllocator<std::pair<const int, value>>
+    > m(std::less<int>{}, alloc);
+
+
+    try{
+        for(int i = 0; i < 100; i++)
+        {
+            m[i] = value();
+            std::cout << "used " << i << " - " <<alloc.pool_->used << std::endl;
+        }
+    } catch (std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+
+
+    return 0;
+}
---
+++ src/MyMapAllocator.hpp
@@ -0,0 +1,72 @@
+//
+// Created by Sg on 07.02.2026.
+//
+
+#pragma once
+#include <map>
+#include <cstddef>
+#include <new>
+
+
+struct Pool {
+
+    std::byte* buffer;
+    size_t used;
+    size_t capacity;
+
+    ~Pool() {
+        ::operator delete(buffer);
+    }
+
+};
+
+template<typename T>
+class MyMapAllocator {
+
+public:
+
+    using value_type = T;
+
+    std::shared_ptr<Pool> pool_;
+
+    template<class U>
+    explicit MyMapAllocator(const MyMapAllocator<U>& other) noexcept
+            : pool_(other.pool_) {}
+
+    explicit MyMapAllocator(size_t capacity);
+    ~MyMapAllocator();
+    T* allocate(size_t n);
+    void deallocate(T* p, std::size_t n) noexcept {};
+};
+
+
+template<typename T>
+MyMapAllocator<T>::~MyMapAllocator() = default;
+
+template<typename T>
+MyMapAllocator<T>::MyMapAllocator(size_t capacity)   {
+
+    pool_ = std::make_shared<Pool>();
+    pool_->capacity = capacity;
+    pool_->used = 0;
+    pool_->buffer = static_cast<std::byte *>(::operator new(capacity));
+}
+
+template<typename T>
+T* MyMapAllocator<T>::allocate(size_t n)
+{
+    std::size_t space = pool_->capacity - pool_->used;
+    void* ptr = pool_->buffer + pool_->used;
+
+    if (!std::align(alignof(T), n * sizeof(T), ptr, space))
+        throw std::bad_alloc{};
+
+    pool_->used = pool_->capacity - space + n * sizeof(T);
+    return static_cast<T*>(ptr);
+}
+
+
+
+
+
+
